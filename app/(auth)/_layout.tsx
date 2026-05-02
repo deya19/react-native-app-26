60f1248db6a5689f42ba991ce8e8b0c5c3c803ff
deya19 <deyasaleh2@gmail.com>
2026-05-02 20:49:38 +0300

    solve some problem

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,22 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect, Stack } from "expo-router";
 import React from "react";
-import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
-export default function AuthLayout() {
+export default function AuthRoutesLayout() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center bg-background">
+        <ActivityIndicator size="large" color="#ea7a53" />
+      </View>
+    );
+  }
+
+  if (isSignedIn) {
+    return <Redirect href="/(tab)" />;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
